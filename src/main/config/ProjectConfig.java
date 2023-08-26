4e1b82579f998d1c0013296120410eef58e03613
Tatrid <nr11seva@gmail.com>
2023-08-26 12:05:35 +0300

    First Commit

---
+++ src/main/config/ProjectConfig.java
@@ -0,0 +1,11 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+@Component
+@ComponentScan(basePackages = {""})
+@EnableAspectJAutoProxy
+public class ProjectConfig {
+}
